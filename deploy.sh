5500c1183f5d5564bd1902bda0501543a3134a35
prasetyarahmad <105980488+prasetyarahmad@users.noreply.github.com>
2022-06-05 22:02:21 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ cd dist
 # echo 'www.example.com' > CNAME
 
 git init
-git checkout -b main
+git checkout -b master
 git add -A
 git commit -m 'deploy'
 
@@ -19,6 +19,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:prasetyarahmad/sinaucoding5.git main:gh-pages
+git push -f git@github.com:prasetyarahmad/sinaucoding5.git master:gh-pages
 
 cd -
